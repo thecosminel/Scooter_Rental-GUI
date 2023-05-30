78c516fb1bccfaa846d1efa075500a8c3b22c1c8
George <thecosminel@gmail.com>
2023-05-30 14:03:16 +0300

    Directories created Domain defined and implemented

---
+++ Domain/Scooter.cpp
@@ -0,0 +1,112 @@
+#include "Scooter.h"
+
+#include <utility>
+using scooter::ScooterStatus;
+
+// Constructors & Destructors
+scooter::Scooter::Scooter()
+{
+    this->identifier = "";
+    this->model = "";
+    this->manufacturingDate = "";
+    this->kilometers = 0.0;
+    this->location = "";
+    this->status = PARKED;
+}
+
+scooter::Scooter::Scooter(string identifier, string model, string date, double kilometers, string location, ScooterStatus status)
+{
+    this->identifier = std::move(identifier);
+    this->model = std::move(model);
+    this->manufacturingDate = std::move(date);
+    this->kilometers = kilometers;
+    this->location = std::move(location);
+    this->status = status;
+}
+
+scooter::Scooter::Scooter(const scooter::Scooter &scooter)
+{
+    this->identifier = scooter.identifier;
+    this->model = scooter.model;
+    this->manufacturingDate = scooter.manufacturingDate;
+    this->location = scooter.location;
+    this->status = scooter.status;
+    this->kilometers = scooter.kilometers;
+}
+
+scooter::Scooter &scooter::Scooter::operator=(const Scooter &scooter){
+    if (this == &scooter)
+        return *this;
+    this->identifier = scooter.identifier;
+    this->model = scooter.model;
+    this->manufacturingDate = scooter.manufacturingDate;
+    this->location = scooter.location;
+    this->status = scooter.status;
+    this->kilometers = scooter.kilometers;
+    return *this;
+}
+
+
+// Getters
+string scooter::Scooter::getIdentifier() const
+{
+    return this->identifier;
+}
+
+string scooter::Scooter::getModel() const
+{
+    return this->model;
+}
+
+string scooter::Scooter::getDate() const
+{
+    return this->manufacturingDate;
+}
+
+double scooter::Scooter::getKilometers() const
+{
+    return this->kilometers;
+}
+
+string scooter::Scooter::getLocation() const
+{
+    return this->location;
+}
+
+ScooterStatus scooter::Scooter::getStatus() const
+{
+    return this->status;
+}
+
+
+// Setters
+void scooter::Scooter::setModel(string newModel)
+{
+    this->model = std::move(newModel);
+}
+
+void scooter::Scooter::setDate(string newDate)
+{
+    this->manufacturingDate = std::move(newDate);
+}
+
+void scooter::Scooter::setKilometers(double newKilometers)
+{
+    this->kilometers = newKilometers;
+}
+
+void scooter::Scooter::setLocation(string newLocation) //NOLINT
+{
+    this->location = std::move(newLocation);
+}
+
+void scooter::Scooter::setStatus(ScooterStatus newStatus)
+{
+    this->status = newStatus;
+}
+
+// Utils
+bool scooter::Scooter::checkIfSameIdentifier(const Scooter &scooter)
+{
+    return this->identifier == scooter.identifier;
+}
---
+++ Domain/Scooter.h
@@ -0,0 +1,83 @@
+#ifndef SCOOTER_RENTAL_GUI_SCOOTER_H
+#define SCOOTER_RENTAL_GUI_SCOOTER_H
+
+#include <string>
+
+using std::string, std::move;
+
+namespace scooter
+{
+    enum ScooterStatus {
+        PARKED,
+        RESERVED,
+        IN_USE,
+        MAINTENANCE,
+        OUT_OF_SERVICE,
+        UNKNOWN
+    };
+
+    class Scooter{
+    private:
+        string identifier;
+        string model;
+        string manufacturingDate;
+        double kilometers;
+        string location;
+        ScooterStatus status;
+
+    public:
+        /// Default constructor
+        Scooter();
+
+        /// Constructor with parameters
+        Scooter(string identifier, string model, string manufacturingDate, double kilometers, string location, ScooterStatus status);
+
+        Scooter(const Scooter &scooter);
+
+        Scooter& operator=(const Scooter &scooter);
+
+        /// Destructor
+        ~Scooter()=default;
+
+        /// Getter method for the scooter identifier
+        [[nodiscard]] string getIdentifier() const;
+
+        /// Getter method for the scooter model
+        [[nodiscard]] string getModel() const;
+
+        /// Getter method for the scooter's date of commissioning
+        [[nodiscard]] string getDate() const;
+
+        /// Getter method for the kilometers traveled by the scooter
+        [[nodiscard]] double getKilometers() const;
+
+        /// Getter method for the scooter's last known location
+        [[nodiscard]] string getLocation() const;
+
+        /// Getter method for the scooter's status
+        [[nodiscard]] ScooterStatus getStatus() const;
+
+        /// Setter method for the scooter model
+        void setModel(string model);
+
+        /// Setter method for the scooter's date of commissioning
+        void setDate(string manufacturingDate);
+
+        /// Setter method for the kilometers traveled by the scooter
+        void setKilometers(double kilometers);
+
+        /// Setter method for the scooter's last known location
+        void setLocation(string location);
+
+        /// Setter method for the scooter's status
+        void setStatus(ScooterStatus status);
+
+        /// Checks if two scooters have the same identifier
+        /// \param scooter
+        /// \return True if they do
+        bool checkIfSameIdentifier(const Scooter& scooter);
+    };
+}
+
+
+#endif //SCOOTER_RENTAL_GUI_SCOOTER_H
